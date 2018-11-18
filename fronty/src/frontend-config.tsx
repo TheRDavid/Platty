011c1142ed04f89a0b7932d096f127b7e4187375
David Rosenbusch <david.rosenbusch@googlemail.com>
2018-11-18 23:14:46 +0000

    remove specific config file

--- fronty/src/frontend-config.tsx
+++
@@ -1,6 +0,0 @@
-export namespace frontendConfig {
-  export var backendAddress: string = "http://localhost:4576/";
-  export var windowWidth: number = 1360,
-    windowHeight = 960;
-  export var appTitle = "Learny";
-}
